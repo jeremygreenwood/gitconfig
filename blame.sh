c257b1da912032ee60b4a00ff373e34287ed1e08
Jeremy Greenwood <jeremy.greenwood@garmin.com>
2015-02-18 11:19:05 -0800

    Updated blame script to support blaming an entire file and strategy to call from gitconfig alias.

--- blame.sh
+++ blame.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-# Note: An unsuccessful attempt was made to convert to single line git alias function using the following command:
-#   cat blame.sh | grep . | sed 's/ \+/ /' | awk 'NR>3' ORS='; ' | sed -e 's/\"/\\"/g'
+# Usage: blame.sh <filename> [start] [end]
+# Notes: it is suggested to create a symbolic link in user home directory pointing to this file
 
 COLOR_CYAN="\e[0;36m"
 COLOR_NC="\e[0m"
+WHOLE_FILE=false
 
 FILE_REGEX="$1"
-START="$2"
+
+if [ $# -gt 1 ]; then
+    START="$2"
+else
+    WHOLE_FILE=true
+fi
+
 if [ $# -gt 2 ]; then
     END="$3"
 else
@@ -15,7 +22,14 @@ fi
 
 for FILE in $( find . -name "${FILE_REGEX}" -printf "%P\n" )
 do
+    if [ ${WHOLE_FILE} == true ]; then
+        FILE_LEN=$( wc -l < "${FILE}" | tr -d '[[:space:]]' )
+        START=1
+        END=${FILE_LEN}
+    fi
+	
     BASH_RSLT="$( git blame "${FILE}" -L ${START},${END} 2> /dev/null )"
+	
     if [ -n "${BASH_RSLT}" ]; then
         echo -e "${COLOR_CYAN}${FILE}${COLOR_NC}"
         echo "${BASH_RSLT}"
